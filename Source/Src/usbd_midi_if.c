3d18633654686729befbd64ab9090a6ba797a7ef
Tim Essig <essig@synyx.de>
2021-07-11 14:59:55 +0200

    Add basic USB-MIDI receive functionality

---
+++ Source/Src/usbd_midi_if.c
@@ -0,0 +1,20 @@
+#include "usbd_midi_if.h"
+
+#include "usb_device.h"
+
+static int8_t MIDI_Receive(uint8_t *Buf, uint32_t Len);
+
+
+USBD_MIDI_CallbacksTypeDef USBD_Interface_Callbacks = {MIDI_Receive};
+
+
+
+int8_t MIDI_Receive(uint8_t *Buf, uint32_t Len) {
+
+	WRITE_REG(GPIOC->BSRR, GPIO_BSRR_BR13); //LED On
+
+	// echo
+	USBD_HID_SendReport(&hUsbDeviceFS, Buf, Len);
+
+	return USBD_OK;
+}
